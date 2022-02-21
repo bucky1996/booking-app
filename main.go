ed6b673c7b920bd9e35b1e871761435cfb149626
Shubham Chidrawar <shubhamxe123@gmail.com>
2022-02-21 17:18:47 +0530

    Added the support for multiple bookings.

--- main.go
+++ main.go
@@ -2,8 +2,56 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	const conferenceName = "Go Conference"
+
+	const ticketCount = 50
+	var remainingTickets = 50
+
+	var bookings = []string{}
+	fmt.Printf("ticket size is %v\n", ticketCount)
+
+	fmt.Println("Welcome to to the ", conferenceName, "booking application")
+
+	fmt.Printf("Type of the ticketCount is %T", ticketCount)
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var tickets int
+
+		fmt.Println("Enter your first Name")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last Name")
+		fmt.Scan(&lastName)
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter the number of tickets you want")
+		fmt.Scan(tickets)
+
+		if remainingTickets > tickets {
+			remainingTickets = ticketCount - tickets
+		}
+		bookings = append(bookings, firstName+" "+lastName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			var fname = names[0]
+			firstNames = append(firstNames, fname)
+			fmt.Printf("The first names of the booking are: %v\n", firstNames)
+		}
+
+	}
+
 }
